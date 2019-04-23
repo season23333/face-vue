5526c2e74a2c619fcf401c56032e170d76fe394b
1059438020 <1059438020@qq.com>
2019-04-23 23:11:24 +0800

    修改运行端口

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer: {
-        port: 8081,     // 端口
+        port: 8080,     // 端口
     },
-    lintOnSave: false   // 取消 eslint 验证
+    lintOnSave: false,   // 取消 eslint 验证
 };
